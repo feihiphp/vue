7f821cc4b87ab29de931afa104e20850c776d643
taofei <1053265279@qq.com>
2018-08-04 17:03:54 +0800

    vue
    自己创建的vue项目

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <input v-model="msg">
+    <input type="text">
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'vue.js'
+    }
+  }
+}
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+</style>
+
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Mock from './util/mock'
+
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  })
